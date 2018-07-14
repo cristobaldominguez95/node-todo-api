0f4eb0e006422cfd6a48478072e3ff5af0539ee0
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-07-14 15:03:32 +0200

    Express setup

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
 var mongoose = require('./utils/db').mongoose;
 var Todo = require('./models/Todo');
 var User = require('./models/User');
 
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
